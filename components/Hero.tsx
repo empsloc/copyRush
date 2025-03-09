b247145251620e03a2524f33a093931e4abeb2ca
vijay7494 <vijaychaure4971@gmail.com>
2025-03-09 18:10:09 +0530

    Centered the section below the header. Page : Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Hero = (props: {}) => {
   return (
-    <div className="max-w-screen-xl px-4 sm:px-6 lg:px-8 grid grid-cols-2 ">
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 grid grid-cols-2 ">
       <div className="flex flex-col justify-center items-start gap-5">
         <h2 className="text-4xl font-bold"> Print Store</h2>
 
