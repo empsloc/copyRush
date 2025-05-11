50f020ec7aec8bbb820f03b601931f01e253c4b6
empsloc <empsloc@gmail.com>
2025-05-11 19:17:19 +0530

    ad

--- components/header.tsx
+++ components/header.tsx
@@ -92,7 +92,7 @@ export const Header = (props: {}) => {
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
-            <ThemeToggleButton/>
+            
               <a
                 className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
                 href="#"
